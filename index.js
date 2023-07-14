56ab8b20c8a3da8bbdafad3127e9c5830fa61565
abdulazizcreator <abdulazizprogrammer@gmail.com>
2023-07-14 15:34:13 +0500

    fadf

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Abdulaziz Programmer");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("*", async (req, res) => {
@@ -11,26 +12,57 @@ app.get("*", async (req, res) => {
   let response = await fetch(endpoint + req.url.split("?")[0]);
   let data = await response.json();
 
+  let totalData = JSON.parse(JSON.stringify(data));
+
   let {
-    query: { page, limit = 10 },
+    query: { page, limit = 10, order },
   } = req;
 
-  console.log(page);
-  console.log(limit);
+  if (order === "asc") {
+    data.sort((a, b) => {
+      const nameA = a.name.common.toUpperCase();
+      const nameB = b.name.common.toUpperCase();
+
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+
+  if (order === "desc") {
+    data.sort((a, b) => {
+      const nameA = a.name.common.toUpperCase();
+      const nameB = b.name.common.toUpperCase();
+
+      if (nameB > nameA) {
+        return 1;
+      }
+
+      if (nameB < nameA) {
+        return -1;
+      }
+
+      return 0;
+    });
+  }
 
   if (page) {
     const startIndex = (page - 1) * limit;
     const endIndex = page * limit;
 
-    const results = {};
-
-    results.results = data.slice(startIndex, endIndex);
-    results.total = data.length;
-
-    res.send(results);
-  } else {
-    res.send(data);
+    data = data.slice(startIndex, endIndex);
   }
+
+  const results = {
+    data,
+    total: totalData.length,
+  };
+
+  res.send(results);
 });
 
 const port = process.env.PORT || 3000;
