0407a5fdaecadf334e4d01730a08dfe39651acdb
abdulazizcreator <abdulazizprogrammer@gmail.com>
2023-07-14 14:37:13 +0500

    fafa

--- index.js
+++ index.js
@@ -36,5 +36,5 @@ app.get("*", async (req, res) => {
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log("Server running on port 3000");
+  console.log(`Server running on port ${port}`);
 });
